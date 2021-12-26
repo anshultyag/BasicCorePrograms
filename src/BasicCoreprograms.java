28071b960fdfc9d235e1706f245418069c4d94c3
anshultyag <you@example.com>
2021-12-26 17:06:57 +0530

    Added: Flipcoin percentage to input a random number

---
+++ src/BasicCoreprograms.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class BasicCoreprograms {
+    public static void main(String[] args) {
+        // INITIALIZATION
+
+        int n, h_cnt = 0, t_cnt = 0;
+        double heads, tails;
+        // Input
+        Scanner sc = new Scanner(System.in);    //System.in is a standard input stream
+        System.out.print("Enter first number- ");
+        int a= sc.nextInt();
+
+        // COMPUTATION
+        for (int i = 0; i < a; i++) {
+            double random = Math.random();
+            if (random < 0.5)
+                t_cnt++;
+            else
+                h_cnt++;
+        }
+
+        heads = h_cnt / (double) a * 100;
+        tails = t_cnt / (double) a * 100;
+        System.out.println("Percentage of heads: " + heads + "%");
+        System.out.println("Percentage of tails: " + tails + "%");
+
+    }
+}
