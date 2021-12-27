e454f9ae5450fc45b8a7bbaa7824f3bb7ca02e29
anshultyag <87016992+anshultyag@users.noreply.github.com>
2021-12-27 13:17:44 +0530

    Update AlphabetOrVowel.java

--- src/AlphabetOrVowel.java
+++ src/AlphabetOrVowel.java
@@ -6,7 +6,7 @@ public class AlphabetOrVowel {
             System.out.println("Enter a character :");
             Scanner sc = new Scanner(System.in);
             char ch = sc.next().charAt(0);
-            if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u'||ch == ' '){
+            if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u'||ch == 'A'||ch == 'I'||ch == 'E'||ch == 'O'||ch == 'U'){
                 System.out.println("Given character is an vowel");
             }else{
                 System.out.println("Given character is a consonant");
